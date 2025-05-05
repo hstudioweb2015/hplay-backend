01e523c993442243e149ed6d43ececc631abfb51
Sebastien-Moraz <sebastien@moraz.net>
2025-05-05 13:08:28 +0200

    feat: add limit for type of error can send to client

--- src/errors/UserError.js
+++ src/errors/UserError.js
@@ -1,4 +1,6 @@
-export default class UserError extends Error {
+import AppError from "./AppError.js";
+
+export default class UserError extends AppError {
 	constructor(messages, status = 400) {
 		super("User operation failed.");
 		this.name = 'UserError';
