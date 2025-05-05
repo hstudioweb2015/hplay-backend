acc065929dc069a4edee9d1cf4811af82c0021e8
Sebastien-Moraz <sebastien@moraz.net>
2025-05-05 14:08:57 +0200

    test: add test for MediaService

---
+++ src/services/MediaService.js
@@ -0,0 +1,9 @@
+export default class MediaService {
+	static async search(name = null, limit = 10, page = 1, tags = []) {
+		throw new Error('Method not implemented');
+	}
+
+	static async get(id) {
+		throw new Error('Method not implemented');
+	}
+}
---
+++ tests/MediaService.test.js
@@ -0,0 +1,105 @@
+import MediaService from '../src/services/MediaService.js';
+import {connectDB} from "../src/configs/db.js";
+import DBService from "../src/services/DBService.js";
+
+function getMedia() {
+	return {
+		name: 'exampleName',
+		description: 'exampleDescription',
+		price: 100,
+		share_id: 1,
+		tags: ['exampleTag']
+	};
+}
+
+beforeEach(async () => {
+	await connectDB();
+	await DBService.query(
+			'INSERT INTO medias (id, name, description, price, share_id) VALUES (?, ?, ?, ?, ?)',
+			[1, 'exampleName', 'exampleDescription', 100, 1],
+			true
+	);
+	await DBService.query(
+			'INSERT INTO tags (id, name) VALUES (?, ?)',
+			[1, 'exampleTag'],
+			true
+	);
+	await DBService.query(
+			'INSERT INTO medias_has_tags (media_id, tag_id) VALUES (?, ?)',
+			[1, 1],
+	);
+});
+
+afterEach(async () => {
+	await DBService.query('DELETE FROM medias_has_tags WHERE media_id = ? AND tag_id = ?', [1, 1]);
+	await DBService.query('DELETE FROM tags WHERE id = ?', [1]);
+	await DBService.query('DELETE FROM medias WHERE id = ?', [1]);
+});
+
+describe('Media Service', () => {
+	it('should search medias', async () => {
+		// Given
+		const name = 'exampleName';
+		const limit = 10;
+		const page = 1;
+		const tags = ['exampleTag'];
+		const media = getMedia();
+
+		// When
+		const response = await MediaService.search(name, limit, page, tags);
+		// Then
+		expect(response).toHaveProperty('total', 1);
+		expect(response).toHaveProperty('page', 1);
+		expect(response).toHaveProperty('limit', 10);
+		expect(response).toHaveProperty('medias');
+		expect(response.medias).toHaveLength(1);
+		expect(response.medias[0]).toHaveProperty('id', media.id);
+	});
+
+	it('should not find any media', async () => {
+		// Given
+		const name = 'nonExistingName';
+		const limit = 10;
+		const page = 1;
+		const tags = ['nonExistingTag'];
+
+		// When
+		const response = await MediaService.search(name, limit, page, tags);
+
+		// Then
+		expect(response).toHaveProperty('total', 0);
+		expect(response).toHaveProperty('page', 1);
+		expect(response).toHaveProperty('limit', 10);
+		expect(response).toHaveProperty('medias');
+		expect(response.medias).toHaveLength(0);
+	});
+
+	it('should get a media by id', async () => {
+		// Given
+		const id = 1;
+		const media = getMedia();
+
+		// When
+		const response = await MediaService.get(id);
+
+		// Then
+		expect(response).toHaveProperty('id', media.id);
+		expect(response).toHaveProperty('name', media.name);
+		expect(response).toHaveProperty('description', media.description);
+		expect(response).toHaveProperty('price', media.price);
+		expect(response).toHaveProperty('share_id', media.share_id);
+		expect(response).toHaveProperty('tags');
+		expect(response.tags).toEqual(media.tags);
+	});
+
+	it('should not find any media by id', async () => {
+		// Given
+		const id = 999;
+
+		// When
+		const response = MediaService.get(id);
+
+		// Then
+		await expect(response).rejects.toThrow('Media not found');
+	});
+});
