bf128352daba18c63a98c4e5313f8c5191cfec00
Sebastien Moraz <sebastien@moraz.net>
2025-05-06 14:40:25 +0200

    fix: fix case sensitive for Authorization parameter in authenticateToken

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -3,8 +3,13 @@ import {jwtSecret} from "../configs/config.js";
 import UserError from "../errors/UserError.js";
 
 export default function authenticateToken(req, res, next) {
-	const authHeader = req.headers["authorization"];
-	const token = authHeader && authHeader.split(" ")[1];
+	const authHeader = req.headers["authorization"] || req.headers["Authorization"];
+	let token = null;
+	if (authHeader.includes("Bearer")) {
+		token = authHeader && authHeader.split(" ")[1];
+	} else {
+		token = authHeader;
+	}
 	if (token == null) next(new UserError("No token provided", 401));
 	jwt.verify(token, jwtSecret, (err, user) => {
 		if (err) next(new UserError("Unauthorized", 401));
