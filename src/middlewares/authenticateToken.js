a3425e5d1c9855bba3dc18ac067b01f809c44573
Sebastien Moraz <sebastien@moraz.net>
2025-05-06 15:52:07 +0200

    fix: repare exception if token is empty in authenticateToken

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -4,13 +4,13 @@ import UserError from "../errors/UserError.js";
 
 export default function authenticateToken(req, res, next) {
 	const authHeader = req.headers["authorization"] || req.headers["Authorization"];
+	if (authHeader == null) next(new UserError("No token provided", 401));
 	let token = null;
 	if (authHeader.includes("Bearer")) {
 		token = authHeader && authHeader.split(" ")[1];
 	} else {
 		token = authHeader;
 	}
-	if (token == null) next(new UserError("No token provided", 401));
 	jwt.verify(token, jwtSecret, (err, user) => {
 		if (err) next(new UserError("Unauthorized", 401));
 		req.user = user;
