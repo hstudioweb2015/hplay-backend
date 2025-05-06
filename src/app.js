77948cb7b32018b5ba099e9f1d539f9723e46a94
Sebastien Moraz <sebastien@moraz.net>
2025-05-06 14:37:41 +0200

    feat: init swagger and implement it

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ import compression from 'compression';
 import logger from "./middlewares/logger.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import routes from './routes/routes.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+
+const swaggerDocument = YAML.load(path.join(path.dirname(fileURLToPath(import.meta.url)), '../swagger.yaml'));
 
 const app = express();
 
@@ -16,6 +23,13 @@ app.use(logger);
 
 // Routes
 app.use('/v1', routes);
+
+// Swagger, load swagger.yaml file
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+	explorer: true,
+	customCss: '.swagger-ui .topbar { display: none }',
+	}));
+	
 
 app.use(errorHandler);
 
