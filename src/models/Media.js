774e333c74ff4880860092b5a1aad089dc9d676b
Sebastien-Moraz <sebastien@moraz.net>
2025-05-07 14:37:19 +0200

    refactor: add comment in Media

--- src/models/Media.js
+++ src/models/Media.js
@@ -1,4 +1,14 @@
 export default class Media {
+	/**
+	 * @constructor
+	 * @description Media model
+	 * @param id {Integer} - Media id
+	 * @param name {String} - Media name
+	 * @param description {String} - Media description
+	 * @param price {Number} - Media price (in cents)
+	 * @param shareId {String} - Media share id
+	 * @param tags {Array} - Media tags
+	 */
 	constructor(id, name, description, price, shareId, tags = []) {
 		this.id = id;
 		this.name = name;
