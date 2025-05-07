04205a2dfeb6f56fe45064dcacbb952fe3589de9
Sebastien-Moraz <sebastien@moraz.net>
2025-05-07 14:34:44 +0200

    refactor: add comment in MediaController

--- src/controllers/MediaController.js
+++ src/controllers/MediaController.js
@@ -1,6 +1,13 @@
 import MediaService from "../services/MediaService.js";
 
 export default class MediaController {
+	/**
+	 * Search for medias
+	 * @param req
+	 * @param res
+	 * @param next
+	 * @returns {Promise<void>}
+	 */
 	static async searchMedia(req, res, next) {
 		try {
 			const response = await MediaService.search(req.body);
@@ -10,6 +17,13 @@ export default class MediaController {
 		}
 	}
 
+	/**
+	 * Get media by id
+	 * @param req
+	 * @param res
+	 * @param next
+	 * @returns {Promise<void>}
+	 */
 	static async getMediaById(req, res, next) {
 		try {
 			const response = await MediaService.get(req.params.id);
@@ -19,6 +33,13 @@ export default class MediaController {
 		}
 	}
 
+	/**
+	 * request a url with unique token to play a media
+	 * @param req
+	 * @param res
+	 * @param next
+	 * @returns {Promise<void>}
+	 */
 	static async playMedia(req, res, next) {
 		try {
 			const response = await MediaService.play(req.body, req.user);
