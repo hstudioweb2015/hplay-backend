1e17c194b325c4bd3794259136fab70b57d02831
Sebastien-Moraz <sebastien@moraz.net>
2025-05-27 08:58:29 +0200

    fix: repare authenticatTokenIfExist

--- src/middlewares/authenticateTokenIfExist.js
+++ src/middlewares/authenticateTokenIfExist.js
@@ -4,16 +4,20 @@ import {jwtSecret} from "../configs/config.js";
 
 export default function authenticateTokenIfExist(req, res, next) {
 	const authHeader = req.headers["authorization"] || req.headers["Authorization"];
-	if (authHeader == null) next();
-	let token = null;
-	if (authHeader.includes("Bearer")) {
-		token = authHeader && authHeader.split(" ")[1];
+	if (authHeader == null) {
+		// If no token is provided, simply call next without setting req.user
+		return next();
 	} else {
-		token = authHeader;
+		let token = null;
+		if (authHeader.includes("Bearer")) {
+			token = authHeader && authHeader.split(" ")[1];
+		} else {
+			token = authHeader;
+		}
+		jwt.verify(token, jwtSecret, (err, user) => {
+			if (err) next(new UserError("Unauthorized", 401));
+			req.user = user;
+			next();
+		});
 	}
-	jwt.verify(token, jwtSecret, (err, user) => {
-		if (err) next(new UserError("Unauthorized", 401));
-		req.user = user;
-		next();
-	});
 }
